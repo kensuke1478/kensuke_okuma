602fe01b09fc8ddc3b535a2809e8335a86ca727a
kensuke <miotyan_shade@yahoo.co.jp>
2021-04-16 22:24:18 +0900

    up

--- sengoku03/sengoku02/sengoku/kensuke_okuma/js/main.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    /**
-    * 画面固定
-    */
-    window.open("javascript:window.addEventListener('resize', function (e) {window.resizeTo(300, 300); }, false);", "Good Luck.", "top=０, left=０, width=1920, height=5000");
-});
